dfd640b66f2ec76d3bcdbd3ce6fd76d6428802c9
daihuan0612 <daihuan0612@gmail.com>
2026-07-12 11:49:22 +0800

    fix: use regex to extract D1 UUID from wrangler d1 list (works with table formatting)

---
+++ public/assets/login-N91xxO9R.js
@@ -0,0 +1,1 @@
+import{m as o,j as m}from"./card-wXeRjWqE.js";import{L as r}from"./login-page-Bf5Uqz37.js";import"./turnstile-BxjzxSMq.js";import"./label-BeYN7X2v.js";o("root",m.jsx(r,{}));
--- public/assets/login-UvR09CyH.js
+++
@@ -1,1 +0,0 @@
-import{m as o,j as m}from"./card-BBfkQP9U.js";import{L as r}from"./login-page-CdlcWHu2.js";import"./turnstile-CJ3MmuR7.js";import"./label-FfS-kP2R.js";o("root",m.jsx(r,{}));
